546242cbd6b14b0e78c516318fc0f14ff2d9db01
Robert Marko <robimarko@gmail.com>
2021-12-28 20:36:45 +0100

    clk: qcom: Add DT bindings for IPQ8074 APSS clock controller
    Add DT-binding for the IPQ8074 APSS clock controller.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>

---
+++ include/dt-bindings/clock/qcom,apss-ipq8074.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2021, The Linux Foundation. All rights reserved.
+ */
+
+#ifndef _DT_BINDINGS_CLOCK_QCA_APSS_IPQ8074_H
+#define _DT_BINDINGS_CLOCK_QCA_APSS_IPQ8074_H
+
+#define APSS_PLL_EARLY				0
+#define APSS_PLL				1
+#define APCS_ALIAS0_CLK_SRC			2
+#define APCS_ALIAS0_CORE_CLK			3
+
+#endif
