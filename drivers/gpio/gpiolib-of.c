37856d6455040cca8375b03cd8397833ce6c25d2
Thierry Reding <treding@nvidia.com>
2022-11-11 12:37:32 +0100

    gpiolib: of: Use correct fwnode for DT-probed chips
    The OF node store in chip->fwnode is used to explicitly override the FW
    node for a GPIO chip. For chips that use the default FW node (i.e. that
    of their parent device), this will be NULL and cause the chip not to be
    fully registered.
    
    Instead, use the GPIO device's FW node, which is set to either the node
    of the parent device or the explicit override in chip->fwnode.
    
    Fixes: 8afe82550240 ("gpiolib: of: Prepare of_gpiochip_add() / of_gpiochip_remove() for fwnode")
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Thierry Reding <treding@nvidia.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpiolib-of.c
+++ drivers/gpio/gpiolib-of.c
@@ -1067,7 +1067,7 @@ int of_gpiochip_add(struct gpio_chip *chip)
 	struct device_node *np;
 	int ret;
 
-	np = to_of_node(chip->fwnode);
+	np = to_of_node(dev_fwnode(&chip->gpiodev->dev));
 	if (!np)
 		return 0;
 
