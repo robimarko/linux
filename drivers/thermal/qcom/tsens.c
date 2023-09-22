af596119cf05acbcc35d2eab3101f23fd2682399
Sricharan Ramabadhran <quic_srichara@quicinc.com>
2023-09-22 17:21:15 +0530

    thermal/drivers/tsens: Add support for IPQ5018 tsens
    IPQ5018 has tsens IP V1.0, 4 sensors and 1 interrupt.
    The soc does not have a RPM, hence tsens has to be reset and
    enabled in the driver init. Adding the driver support for same.
    
    Signed-off-by: Sricharan Ramabadhran <quic_srichara@quicinc.com>

--- drivers/thermal/qcom/tsens.c
+++ drivers/thermal/qcom/tsens.c
@@ -1102,6 +1102,9 @@ static SIMPLE_DEV_PM_OPS(tsens_pm_ops, tsens_suspend, tsens_resume);
 
 static const struct of_device_id tsens_table[] = {
 	{
+		.compatible = "qcom,ipq5018-tsens",
+		.data = &data_ipq5018,
+	}, {
 		.compatible = "qcom,ipq8064-tsens",
 		.data = &data_8960,
 	}, {
