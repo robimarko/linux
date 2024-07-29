3811cb377b0cc553d0cb952e2b16cdcce5118bbe
Robert Marko <robimarko@gmail.com>
2024-07-29 13:40:02 +0200

    power: apm: Convert to platform remove callback returning void
    The .remove() callback for a platform driver returns an int which makes
    many driver authors wrongly assume it's possible to do error handling by
    returning an error code. However the value returned is ignored (apart
    from emitting a warning) and this typically results in resource leaks.
    
    To improve here there is a quest to make the remove callback return
    void. In the first step of this quest all drivers are converted to
    .remove_new(), which already returns void. Eventually after all drivers
    are converted, .remove_new() will be renamed to .remove().
    
    Trivially convert this driver from always returning zero in the remove
    callback to the void returning variant.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- drivers/power/qcom/apm.c
+++ drivers/power/qcom/apm.c
@@ -900,7 +900,7 @@ static int msm_apm_probe(struct platform_device *pdev)
 	return ret;
 }
 
-static int msm_apm_remove(struct platform_device *pdev)
+static void msm_apm_remove(struct platform_device *pdev)
 {
 	struct msm_apm_ctrl_dev *ctrl_dev;
 
@@ -911,8 +911,6 @@ static int msm_apm_remove(struct platform_device *pdev)
 		mutex_unlock(&apm_ctrl_list_mutex);
 		apm_debugfs_deinit(ctrl_dev);
 	}
-
-	return 0;
 }
 
 static struct platform_driver msm_apm_driver = {
