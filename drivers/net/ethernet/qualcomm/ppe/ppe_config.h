34cdbf40c158944f201b556656b484a5907eec94
Luo Jie <quic_luoj@quicinc.com>
2025-01-08 21:47:11 +0800

    net: ethernet: qualcomm: Initialize PPE buffer management for IPQ9574
    The BM (Buffer Management) config controls the pause frame generated
    on the PPE port. There are maximum 15 BM ports and 4 groups supported,
    all BM ports are assigned to group 0 by default. The number of hardware
    buffers configured for the port influence the threshold of the flow
    control for that port.
    
    Signed-off-by: Luo Jie <quic_luoj@quicinc.com>

---
+++ drivers/net/ethernet/qualcomm/ppe/ppe_config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only
+ *
+ * Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef __PPE_CONFIG_H__
+#define __PPE_CONFIG_H__
+
+#include "ppe.h"
+
+int ppe_hw_config(struct ppe_device *ppe_dev);
+#endif
