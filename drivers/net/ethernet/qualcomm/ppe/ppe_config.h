51d1ee4d06034c82a204b398f5c2fdecbcc1ae3e
Luo Jie <quic_luoj@quicinc.com>
2025-01-08 21:47:16 +0800

    net: ethernet: qualcomm: Initialize PPE port control settings
    1. Enable port specific counters in PPE.
    2. Configure the default action as drop when the packet size
       is more than the configured MTU of physical port.
    
    Signed-off-by: Luo Jie <quic_luoj@quicinc.com>

--- drivers/net/ethernet/qualcomm/ppe/ppe_config.h
+++ drivers/net/ethernet/qualcomm/ppe/ppe_config.h
@@ -233,6 +233,16 @@ struct ppe_sc_cfg {
 	int eip_offset_sel;
 };
 
+/* The action of packet received by PPE can be forwarded, dropped, copied
+ * to CPU (enter multicast queue), redirected to CPU (enter unicast queue).
+ */
+enum ppe_action_type {
+	PPE_ACTION_FORWARD = 0,
+	PPE_ACTION_DROP = 1,
+	PPE_ACTION_COPY_TO_CPU = 2,
+	PPE_ACTION_REDIRECT_TO_CPU = 3,
+};
+
 int ppe_hw_config(struct ppe_device *ppe_dev);
 int ppe_queue_scheduler_set(struct ppe_device *ppe_dev,
 			    int node_id, bool flow_level, int port,
@@ -254,4 +264,5 @@ int ppe_port_resource_get(struct ppe_device *ppe_dev, int port,
 			  int *res_start, int *res_end);
 int ppe_sc_config_set(struct ppe_device *ppe_dev, int sc,
 		      struct ppe_sc_cfg cfg);
+int ppe_counter_enable_set(struct ppe_device *ppe_dev, int port, bool enable);
 #endif
